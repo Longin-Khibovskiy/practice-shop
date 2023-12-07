9713f484c8f91a50ddbf90a2227c8624cff97319
Longin <l.khibovskiy@gmail.com>
2023-12-07 23:41:27 +0300

    add price in cart

--- cart/forms.py
+++ cart/forms.py
@@ -5,8 +5,6 @@ from django.forms import NumberInput
 class CartAddProductForm(forms.Form):
     quantity = forms.IntegerField(
         min_value=1,
-        widget=NumberInput(attrs={
-            'class': 'form-control text-center px-3',
-            'value': 1
-        })
+        initial=1,
+        widget=forms.HiddenInput()
     )
