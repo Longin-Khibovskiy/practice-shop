02cef1000e14ddb036ce9169fe778fad5862d4ab
Longin <l.khibovskiy@gmail.com>
2023-12-09 01:27:44 +0300

    the checkout page style

--- orders/forms.py
+++ orders/forms.py
@@ -5,9 +5,18 @@ from .models import Order
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email',
-                  'address', 'postal_code']
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code']
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'email': 'Email',
+            'address': 'Адрес',
+            'postal_code': 'Почтовый индекс',
+        }
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
-            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+            'first_name': forms.TextInput(attrs={'class': 'input'}),
+            'last_name': forms.TextInput(attrs={'class': 'input'}),
+            'email': forms.EmailInput(attrs={'class': 'input'}),
+            'address': forms.TextInput(attrs={'class': 'input'}),
+            'postal_code': forms.TextInput(attrs={'class': 'input'}),
         }
